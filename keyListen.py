a132e72544b9efe1cc8a4787a0f74934b48c5273
Milan <milan95@gmail.com>
2014-10-04 15:12:11 -0400

    fixed socket sendto case

--- keyListen.py
+++ keyListen.py
@@ -6,4 +6,4 @@ UDP_PORT = 5005
 MESSAGE = "Hello, world!"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendTo(MESSAGE, (UDP_PORT,UDP_IP))
+sock.sendto(MESSAGE, (UDP_IP,UDP_PORT))
--- spheroComm.py
+++ spheroComm.py
@@ -7,7 +7,8 @@ import socket
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
