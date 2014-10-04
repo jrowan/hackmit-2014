9b015b6789bfe7ddd0919286be2d16e5d2677388
Milan <milan95@gmail.com>
2014-10-04 14:26:56 -0400

    finally

---
+++ spheroComm.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import bluetooth
+import struct
+import time
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+		data, addr = sock.recvfrom(1024)
+		print "received message:", data
+
+from sphero_driver import sphero_driver
+import sys
+sphero = sphero_driver.Sphero()
+sphero.connect()
+sphero.set_raw_data_strm(40, 1 , 0, False)
+
+sphero.start()
+
+time.sleep(2)
+sphero.roll(255,0,1,False)
+time.sleep(2)
+sphero.roll(0,0,0,False)
+sphero.join()
+sphero.disconnect()
+sys.exit(1)
