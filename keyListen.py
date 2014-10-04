5674ffd821a4dd779b1e51affca633a1930cfdbe
Milan <milan95@gmail.com>
2014-10-04 15:47:23 -0400

    changed port

--- keyListen.py
+++ keyListen.py
@@ -2,7 +2,7 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5000
 MESSAGE = "Hello, world!"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
--- spheroComm.py
+++ spheroComm.py
@@ -5,7 +5,7 @@ import time
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_PORT = 5000
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
